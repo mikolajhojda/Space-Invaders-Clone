e65e6fdf1526c3fdc91ec112f7ba23f804bd0b37
mikolajhojda <kolaho58@gmail.com>
2018-10-19 22:54:40 +0200

    First Commit

---
+++ Space Invaders/Assets/Scripts/BulletMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour {
+
+	//ruch przeciwnika
+
+	void Start () {
+		
+	}
+    public float speed = 40f;
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0, 1, 0) * speed * Time.deltaTime);
+
+        /*void OnTriggerEnter2D(Collider2D hitinfo)
+        {
+            Debug.Log(hitinfo.name);
+            Destroy(gameObject);
+        }*/
+	}
+}
---
+++ Space Invaders/Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float speed = 5.0f;
+    
+    void Update()
+    {
+
+        //poruszanie sie
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0);
+        transform.position += move * speed * Time.deltaTime;
+        if ((transform.position.x > 9.5)||(transform.position.x < -9.5))
+        {
+            transform.position -= move * speed * Time.deltaTime;
+        }
+    }
+}
