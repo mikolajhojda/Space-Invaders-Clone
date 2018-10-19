2d556db9fc71bce6b4b586aeaca4ce8528c4e851
mikolajhojda <kolaho58@gmail.com>
2018-10-19 23:00:19 +0200

    First Commit

---
+++ Space Invaders/Assets/Scripts/Shoot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bullet;
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{  
+			Destroy(Instantiate(bullet, transform.position, transform.rotation), 2); 
+		}
+	}
+}
